f5ea8d7c49862b99099674706b4e166141b2ddfa
JiRuD0 <88602443+JiRuD0@users.noreply.github.com>
2021-08-09 09:49:33 +0900

    変更

---
+++ load.js
@@ -0,0 +1,20 @@
+function load(){
+    var data_ary;
+    $.ajax({
+          url: "./test.txt",
+          type: "get",
+          success : function(data) {
+              /* data_ary = data.split("\n"); */
+              data_ary = data;
+             var md = marked(data_ary);
+             $("#container").append(md);
+          },
+          error:function(data) {
+              alert("申し訳ありません。読み込みに失敗しました。");
+          }
+    })
+    
+  //テキスト形式で読み込む
+  
+             //$(".container").append("bbb".toString());
+  }
